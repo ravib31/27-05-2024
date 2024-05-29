f03bbeca7499c87a2f325d7b018acaed362bf54d
atheist31 <rkb0808@gmaol.com>
2024-05-29 13:45:32 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // require ("dotenv").config({path:"./env"})
 import dotenv from "dotenv"
+import { app } from "./app.js";
 import connectedDB from "./db/index.js";
 dotenv.config({
     path:"./env"
@@ -8,6 +9,14 @@ dotenv.config({
 
 
 connectedDB()
+.then(()=>{
+    app.listen(process.env.PORT || 5000,()=>{
+        console.log(`Hye buddy !! Server is running on port : ${process.env.PORT}` )
+    } )
+})
+.catch((err)=>{
+    console.log("Connection failed" ,err)
+})
 
 
 
